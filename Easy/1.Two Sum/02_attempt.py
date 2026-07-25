4cc71908376d93b84c482f5fed18fea5a1fdf0aa
Choin Park <choin@Choins-MacBook-Pro.local>
2026-07-25 23:59:29 +0900

    Add LeetCode solutions (0014, 0027)

---
+++ Easy/1.Two Sum/02_attempt.py
@@ -0,0 +1,22 @@
+#================================
+# Working process:
+#   1. Iterate through nums with double loop variable
+#   2. Set comp variable defined as difference of target and list nums's element
+#   3. Return the list of comp and i if comp is in the dictionary memo
+#   4. Else, put that element in the memo as a key
+# Refinement: Shorter runtime by using only one loop
+# TakeAway: Learning how to use library data type
+#================================
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        memo = {}
+
+        for i, num in enumerate(nums):
+            comp = target - num
+
+            if comp in memo:
+                return[memo[comp],i]
+            else:
+                memo[num] = i
