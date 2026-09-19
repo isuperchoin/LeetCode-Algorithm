d820b61b5395365f53cf9f89c1bd26f28ba6d409
Choin <isuperchoin@gmail.com>
2026-09-19 21:33:40 +0900

    Leet code question 994 solved

--- Medium/200. Number of Islands/01_attempt.py
+++ Medium/200. Number of Islands/01_attempt.py
@@ -11,7 +11,7 @@
 
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid: list[list[str]]) -> int:
 
         def dfs(r,c):
             if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] == "0":
